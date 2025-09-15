16f5a671cf5fb2f55a14594f233d00042aecad7d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-09-15 12:21:29 -0600

    Update crossword.py

--- crossword.py
+++ crossword.py
@@ -422,7 +422,7 @@ def plot_crossword(a, size):
             else:
                 axs[y,x].set_axis_off()
         axs[word_df.shape[0],x].set_axis_off()
-    axs[word_df.shape[0],0].text(1,0.95,a.legend(),va='top',fontsize=10)
+    axs[word_df.shape[0],0].text(1,0.95,a.legend(),va='top',fontsize=9)
     axs[word_df.shape[0],0].set_axis_off()
     sns.despine(top=False,right=False)
     st.pyplot(fig,use_container_width=False)
