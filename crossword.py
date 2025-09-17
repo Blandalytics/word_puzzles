781ccfab34bb0b9b129ee15a077963bf94d1f87d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-09-17 00:16:36 -0700

    Update crossword.py

--- crossword.py
+++ crossword.py
@@ -437,7 +437,7 @@ def plot_crossword(a, size, checkbox,title):
     if checkbox:
         axs[word_df.shape[0],word_df.shape[1]-6].text(1,0.95,'Word Bank:\n\n'+a.word_bank(),va='top',fontsize=18)
         axs[word_df.shape[0],word_df.shape[1]-6].set_axis_off()
-    fig.suptitle(title,size=30,y=0.9)
+    fig.suptitle(title,size=30,y=0.95)
     sns.despine(top=False,right=False)
     pdf_name = 'crossword.pdf'
     fig.savefig(pdf_name,format='pdf')
